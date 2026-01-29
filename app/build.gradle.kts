852d75a1fb1d9dde3082c262558348f00af00762
nendo <tmgast@gmail.com>
2026-01-30 06:18:18 +0900

    Bump version to 0.18.0-beta.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.nendo.argosy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 154
-        versionName = "0.18.0-beta.10"
+        versionCode = 155
+        versionName = "0.18.0-beta.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
